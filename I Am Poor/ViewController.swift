b32b270788568cd409afccce32845398c3e6797d
Austin Meyer <Austin.a.Meyer@gmail.com>
2020-01-06 18:17:03 -0600

    Final Application

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Austin Meyer on 1/5/20.
+//  Copyright © 2020 Austin Meyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
